af91704c406d2beabf6b90ed1e6e696105382273
Breee <74506708+breeev@users.noreply.github.com>
2022-06-09 21:19:46 +0200

    Add files via upload

--- beepcomp library chord test.py
+++ beepcomp library chord test.py
@@ -2,8 +2,8 @@ from beepcomp import chord
 channels=[f'@{i+1}L1PRESET=BEEPWAVEFORM=0' for i in range(4)]
 def addchord(code):
     for i,note in enumerate(chord(code)):channels[i]+=note
-addchord('G')
+addchord('C')
+addchord('G/B')
 addchord('A#')
-addchord('C')
-addchord('C')
+addchord('F')
 for channel in channels:print(channel)
