d83956a33c4caef908edaa609980e4710ed4ad75
BrevalF <breval.ferrari@gmail.com>
2022-06-13 23:08:03 +0200

    Modify web implementation to add tempo argument

--- chord.py
+++ chord.py
@@ -1,10 +1,11 @@
 from beepcomp import chord
 from sys import argv
 #input='C,Am,Dm,G'
-input=''.join(argv[2:]).replace(' ','')
+input=''.join(argv[3:]).replace(' ','')
 codes=input.split(',')
-channels=[f'@{i+1}L1PRESET=BEEPWAVEFORM={argv[1]}' for i in range(max((len(chord(c)) for c in codes)))]
+channels=[f'@{i+1}L1PRESET=BEEPWAVEFORM={argv[2]}' for i in range(max((len(chord(c)) for c in codes)))]
 def addchord(code):
     for i,note in enumerate(chord(code)):channels[i]+=note.replace('>','')
 for code in codes:addchord(code)
+channels.insert(0,f'@GDELAY=OFFTEMPO={argv[1]}')
 for channel in channels:print(channel,end='')
