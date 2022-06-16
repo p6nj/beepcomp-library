b2b7faafd56ea2e4977bbdaed5732b42c78a4b0f
BrevalF <breval.ferrari@gmail.com>
2022-06-16 22:27:16 +0200

    modified timidity for testing purposes

--- timidity-test.py
+++ timidity-test.py
@@ -1,6 +1,8 @@
 from timidity import Parser, play_notes
-import numpy as np
-
-ps = Parser("D:/dwn/inv1.mid")
-
-play_notes(*ps.parse(), np.sin)
+from scipy import signal
+from glob import glob
+midi_list = glob("C:/Users/breva/Documents/midi/Mario & Luigi (and other Mario games) midis/songs/*.mid")
+for filename in midi_list:
+    ps = Parser(filename)
+    print(filename.split('/')[-1].split('.')[0])
+    play_notes(*ps.parse(),signal.square,basename=filename.split('\\')[-1].split('.')[0])
